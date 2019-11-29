3c612b09cfb1d80ba16f3ca0448e9088987c9fe1
LVLeozao21 <verasleonardo210@gmail.com>
2019-11-29 13:30:54 -0300

    Tela Home

--- meu_site/settings.py
+++ meu_site/settings.py
@@ -120,7 +120,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-LOGIN_REDIRECT_URL = "deliverys"
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "login"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
